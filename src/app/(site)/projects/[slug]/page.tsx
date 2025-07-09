ca79fbda8158a6f0defcdda8fe0d9f9a62c1cb0a
ecardoz05623 <ecardoz05623@universidadean.edu.co>
2025-07-09 16:36:28 -0500

    Actualiza error en projects

--- src/app/(site)/projects/[slug]/page.tsx
+++ src/app/(site)/projects/[slug]/page.tsx
@@ -13,12 +13,12 @@ export async function generateMetadata({ params }: Props) {
 
     const project = getProjectsBySlug(slug, ["title", "ScopeOfWork", "industry", "raised", "website", "description", "coverImage", "gallery", "content"]);
 
-    const siteName = process.env.SITE_NAME || "Your Site Name";
-    const authorName = process.env.AUTHOR_NAME || "Your Author Name";
+    const siteName = process.env.SITE_NAME || "Amerseguros";
+    const authorName = process.env.AUTHOR_NAME || "Amerseguros";
 
     if (project) {
         const metadata = {
-            title: '${project.title || "Single Post Page"} | ${siteName}',
+            title: `${project.title || "Single Post Page"} | ${siteName}`,
             robots: {
                 index: true,
                 follow: true,
